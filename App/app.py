7c95fff448a269ab5c807aa043ad8ea7511583b4
root <root@ChosenDevop.localdomain>
2024-03-18 15:49:15 +0100

    created application routes

---
+++ App/app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def homePage():
+    """
+        returns the home Page
+    """
+    return "Welcome to Mamaput"
+
+@app.route('/login', strict_slashes=False)
+def Login():
+    """
+        returns the login Page
+    """
+    return "Welcome to Login"
+
+@app.route('/signup', strict_slashes=False)
+def signup():
+    """
+        returns the sign Page
+    """
+    return "Welcome to signup"
+
+@app.route('/about', strict_slashes=False)
+def aboutPage():
+    """
+        returns the about Page
+    """
+    return "Welcome to about page"
+
+@app.route('/contactUs', strict_slashes=False)
+def contactUs():
+    """
+        returns the contact us Page
+    """
+    return "Welcome to conatct page"
+
+
+if __name__ == "__main__":
+    """ main function """
+    app.run(host="0.0.0.0", port=5000)
