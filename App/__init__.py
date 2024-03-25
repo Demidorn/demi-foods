47a23f1d41508013759983e14519f5dbd873fcab
Demidorn <dornkaizen@gmail.com>
2024-03-25 15:06:33 +0300

    unrendered updates

--- App/__init__.py
+++ App/__init__.py
@@ -1,26 +1,26 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
 
 
 
 
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
+#def create_app():
+   # app = Flask(__name__)
     
 
-    app.config['SECRET_KEY'] = '123456789'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+   # app.config['SECRET_KEY'] = '123456789'
+  #  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     
     
     
     #from .mamaput import mamaput as auth_blueprint
     #app.register_blueprint(auth_blueprint)
-    from .mamaput import mamaput
-    app.register_blueprint(mamaput)
+    #from .mamaput import mamaput
+    #app.register_blueprint(mamaput)
     
-    db.init_app(app)
+    #db.init_app(app)
     
-    return app
+   # return app
